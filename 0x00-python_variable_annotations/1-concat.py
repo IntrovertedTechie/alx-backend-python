bdf499250e13553723711eedc33e80ecc6c241ed
Adio Adebayo <zillionaire234@gmail.com>
2023-07-06 20:05:58 +0100

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-""" Returns a conc string """
-
-
 def concat(str1: str, str2: str) -> str:
-    """ functions that takes two strings, returns a conc string """
+    
     return (str1 + str2)
