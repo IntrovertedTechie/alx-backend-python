f4009e7da68b748e1c860bed956bd5d6e7ad76af
Adio Adebayo <zillionaire234@gmail.com>
2023-07-07 23:07:15 +0100

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
+
+"""
+This module provides a function for adding two floating-point numbers.
+"""
+
 def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of the two numbers.
+    """
     return a + b
+
