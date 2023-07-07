fc3bb1bc3fc1a88371df68c31912cf486cfb3383
Adio Adebayo <zillionaire234@gmail.com>
2023-07-08 00:09:41 +0100

    Update 3-to_str.py

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-This module provides a function for converting a float to its string representation.
+This module provides a function for converting a given float to its string representation.
 """
 
 def to_str(n: float) -> str:
