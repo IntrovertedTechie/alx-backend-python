5fb162b6930bdee3a475efffb8fe1e4ccddca108
Adio Adebayo <zillionaire234@gmail.com>
2023-07-08 02:46:24 +0100

    Update 101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python3
-from typing import Mapping, Any, Union, TypeVar
+from typing import Mapping, Any, Union, 
+
 
 T = TypeVar('T')
 
+
 def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
     """
-    Returns the value associated with the given key in the dictionary `dct`.
-    If the key is not found, returns the default value.
-
-    Args:
-        dct: A mapping object (e.g., dictionary).
-        key: The key to search for in `dct`.
-        default: The default value to return if the key is not found (default: None).
-
-    Returns:
-        The value associated with the key if found, otherwise the default value.
+    Returns the value associated with
     """
     if key in dct:
         return dct[key]
