0b72c97c7256ceb26175bb44abf3edcd950222e8
Adio Adebayo <zillionaire234@gmail.com>
2023-07-08 01:58:50 +0100

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
 """
-This module provides a function for converting a string 
+This module provides a function for converting a string.
 """
 
 from typing import Union, Tuple
 
-
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    Returns a tuple with the string k as the first element 
+    Returns a tuple with the string k as the first element.
     """
     return k, v ** 2
