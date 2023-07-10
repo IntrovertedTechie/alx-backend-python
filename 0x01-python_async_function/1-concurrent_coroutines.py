af9f28cef0d4d9e0f5a250de9ab9e4fdb7612e1b
Adio Adebayo <zillionaire234@gmail.com>
2023-07-10 23:38:30 +0100

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,18 +5,18 @@ from typing import List
 import random
 
 
-async def wait_random(max_delay: 
- = 10) -> float:
+async def wait_random(max_delay: = 10) -> float:
+
     """
     Asynchronous coroutine .
     """
-    delay: float = random.uniform(0, max_delay)
+    delay:float=random.uniform(0,max_delay)
     await asyncio.sleep(delay)
     return delay
 
 
-async def wait_n(n: 
-, max_delay: int) -> List[float]:
+async def wait_n(n: , max_delay: int) -> List[float]:
+
     """
     Asynchronous coroutine 
     """
