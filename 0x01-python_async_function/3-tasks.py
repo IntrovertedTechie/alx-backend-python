b4a5719d8d30bdd25cdf12bd46a73d0d558e6655
Adio Adebayo <zillionaire234@gmail.com>
2023-07-11 00:49:38 +0100

    Update 3-tasks.py

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -3,12 +3,11 @@
 import asyncio
 from typing import Callable
 
-
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
-    Takes an integer max_delay 
+    Takes an integer max_delay and returns an asyncio.Task.
     """
     wait_random = __import__('0-basic_async_syntax').wait_random
     loop = asyncio.get_event_loop()
-    task: asyncio.Task = loop.create_task(wait_random(max_delay))
+    task = loop.create_task(wait_random(max_delay))
     return task
